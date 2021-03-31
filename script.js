e34987ee1157ab6a1698eed68053c79e4412085c
hafeezullahkakar <hafeezullahkakar@gmail.com>
2021-03-31 14:14:33 +0500

    added loader to the bottom of each page and infinity functionality

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const imageContainer = document.getElementById('img-container');
 const loader = document.getElementById('loader')
 
+let imagesAreReady= false;
+let imagesNoLoaded = 0;
+let totalImages = 0;
 var photoArray =[];
 const count = 10;
 const apiKey = 'BbltDyLd9Ga8fvePQkMixXUJs4XZtIoA7yAL51HFgB0'
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
+//Checking if images are loaded
+function imageLoaded(){
+    imagesNoLoaded++;
+    if(imagesNoLoaded === totalImages){
+        imagesAreReady= true;
+        loader.hidden = true;
+    }
+}
 //Showing each object of photoArray
 function displayPhotos(){
+    imagesNoLoaded = 0;
+    totalImages = photoArray.length;
     photoArray.forEach((photo)=>{     
         // create a to link to unsplash
         const item = document.createElement('a');     
@@ -18,6 +31,8 @@ function displayPhotos(){
         imge.setAttribute('src', photo.urls.regular)
         imge.setAttribute('alt', photo.alt_description)
         imge.setAttribute('title', photo.alt_description)
+        //Check if image is loaded
+        imge.addEventListener('load', imageLoaded)
        // put img inside <a> then put both inisde img-container
         item.appendChild(imge);
         imageContainer.appendChild(item);
@@ -35,4 +50,12 @@ async function getPhoto(){
         console.log("error is: ",error)
     }
 }
+
+//check if scrolling is near bottom
+window.addEventListener('scroll', ()=>{
+        if(window.innerHeight + window.scrollY >= document.body.offsetHeight-1000 && imagesAreReady){
+              imagesAreReady = false;
+              getPhoto();
+        }
+})
 getPhoto();
