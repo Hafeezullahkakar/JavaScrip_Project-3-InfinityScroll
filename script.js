7d4d7702d882de0418b1db6116d9ffcd0be4c431
hafeezullahkakar <hafeezullahkakar@gmail.com>
2021-03-31 13:20:25 +0500

    added displayPhotos function to create image item and  set attributes of fetched images

--- script.js
+++ script.js
@@ -1,54 +1,38 @@
 const imageContainer = document.getElementById('img-container');
+const loader = document.getElementById('loader')
 
-let photoArray =[];
-
+var photoArray =[];
 const count = 10;
 const apiKey = 'BbltDyLd9Ga8fvePQkMixXUJs4XZtIoA7yAL51HFgB0'
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
-
-//Helper function to set Attributes to elements
-function setAttributes(element, attributes){
-    for(const key in attributes){
-        element.setAttributes(key, attributes[key])
-    }
-}
 //Showing each object of photoArray
 function displayPhotos(){
-    photoArray.forEach((photo)=>{
+    photoArray.forEach((photo)=>{     
         // create a to link to unsplash
-        const item = document.getElementById('a');  
-        setAttributes(item, {
-            href: photo.links.html,
-            target: '_blank'
-        })
+        const item = document.createElement('a');     
+        item.setAttribute('href', photo.links.html)
+        item.setAttribute('target', '_blank')
         //create image for photo
-        const img = document.getElementById('img');
-        setAttributes(img,{
-            src: photo.urls.regular,
-            alt: photo.alt.description,
-            title: photo.alt.title
-        })
-        //put img inside <a> then put both inisde img-container
-        item.appendChild(img);
-        imageContainer.appendChild(item);   
-
-
-
-
+        const imge = document.createElement('img')
+        imge.setAttribute('src', photo.urls.regular)
+        imge.setAttribute('alt', photo.alt_description)
+        imge.setAttribute('title', photo.alt_description)
+       // put img inside <a> then put both inisde img-container
+        item.appendChild(imge);
+        imageContainer.appendChild(item);
     })
 }
-
 // Getting Photos from Unsplash API
 async function getPhoto(){
     try{
         const response = await fetch(apiURL);
         photoArray = await response.json();
-
+        // console.log(photoArray)
         displayPhotos();
     }
     catch(error){
-
+        console.log("error is: ",error)
     }
 }
 getPhoto();
